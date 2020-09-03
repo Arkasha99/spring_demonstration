75598834c13e81fd3e4e4d6bab1586c7a69c46bf
Arkasha99 <greyform16@gmail.com>
2020-09-03 18:05:05 +0600

    beginning of the REST controllers

--- src/main/java/com/bazarjoq/blog/Controller/BlogRestController.java
+++ src/main/java/com/bazarjoq/blog/Controller/BlogRestController.java
@@ -1,8 +1,11 @@
 package com.bazarjoq.blog.Controller;
 
 import com.bazarjoq.blog.models.Post;
+import com.bazarjoq.blog.models.User;
 import com.bazarjoq.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +13,43 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("blogapi")
 public class BlogRestController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("blogapi")
+    @GetMapping
     public Iterable<Post> blogMain(Model model){
-        Iterable<Post> posts = postRepository.findAll();
-        return(posts);
+        return(postRepository.findAll());
     }
 
 
-    @GetMapping("blogapi/{id}")
-    public ArrayList<Post> blogDetails(@PathVariable(value = "id") Long id, Model model){
-        Optional<Post> post = postRepository.findById(id);
-        ArrayList<Post> res = new ArrayList<>();
-        post.ifPresent(res::add);
-        return (res);
+    @GetMapping("{id}")
+    public Post blogDetails(@PathVariable(value = "id") Long id){
+        return (postRepository.findById(id).orElse(null));
+    }
+
+    @PostMapping
+    public void addPost(@AuthenticationPrincipal User user,@RequestBody Post post){
+        post.setAuthor(user);
+        postRepository.save(post);
+    }
+
+    @PutMapping("{id}")
+    public void changePost(@PathVariable(value = "id") Long id,@RequestBody Post post){
+        Post changedPost = postRepository.findById(id).orElse(null);
+        changedPost.setText(post.getText());
+        changedPost.setAnnounce(post.getAnnounce());
+        changedPost.setName(post.getName());
+        changedPost.setAuthor(post.getAuthor());
+        changedPost.setId(post.getId());
+        postRepository.save(changedPost);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePost(@PathVariable(value = "id") Long id){
+        Post post = postRepository.findById(id).orElse(null);
+        postRepository.delete(post);
     }
 }
